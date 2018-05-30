6dc1ea6e8e295a37be0298766e1337171c481710
Leonardo <leonardoaraujodf@gmail.com>
2018-05-30 15:14:06 -0300

    Inserted functions gethelp, make_assistant_sleep

--- src/main.c
+++ src/main.c
@@ -6,42 +6,108 @@
 #define MAX_STUDENTS 40
 #define MIN_STUDENTS 3
 
-int *chairs;
+/*This number indicates how many chairs exists in the hall*/
 int number_chairs;
-int lock;
+int chairs_occupied = 0;
+int being_helped;
+int assistant_sleeping = 0;
+int can_sleep = 0;
+/*This variable indicates who (using the student id) is sitting in the assistant_chair and
+  is being helped in the exact moment.
+  If no one is being helped in the moment, the number id is set to -1.*/
+
+int assistant_chair = -1;
+
+sem_t semaphore, sleep;
+pthread_mutex_t mutexLock; 
 
 
-int verify_chair(void){
-	/*
-	- returns the number of an available chair if
-	it's available.
-	
-	- returns -1 if no chair is available
+void go_program(void){
+
+	int program_time = rand() % 10;
+	sleep(program_time);
+}
+
+
+void wakeup_assistant(void){
+	sem_post(&sleep);
+}
+
+void make_assistant_sleep(void){
+	can_sleep = 1;
+	sem_wait(&sleep);
+}
+
+int get_help(int student_id){
+
+	/*  get_help:
+
+	  - Verifies if the assistant chair is free and he is available to help.
+	  - If the assistant chair is occupied, the student should wait in a chair in the hall.
+	  - If the number of chairs occupied is equal to the number of chairs, the function will 
+	  return 0 meaning that there's no chair available anymore, and the student should go away.
+	  - If the student got help, the variable being_helped will be set to 0 by the assistant, and
+	  this function will return 1, meaning that the student got help.
+
 	*/
-	int i;
 
 
-	for(i=0;i<number_chairs;i++){
+	if(chairs_occupied < number_chairs){
 
-		if(chairs[i] == 0){
-			return i;
-		}
+		pthread_mutex_lock(&mutexLock);	
+			chairs_occupied ++;
+		pthread_mutex_unlock(&mutexLock);
+
+		sem_wait(&semaphore);
+			assistant_chair = student_id;
+			being_helped = 1;
+			if(assistant_sleeping == 1){
+				wakeup_assistant();
+			}
+				while(being_helped > 0);
+			make_assistant_sleep();
+			assistant_chair = -1;
+		sem_post(&semaphore);
+
+		pthread_mutex_lock(&mutexLock);
+			chairs_occupied--;
+		pthread_mutex_unlock(&mutexLock);
+		return 1;
+	}
+
+	else{
+		return 0;
 	}
 
 }
 
 
-void std_assistant(void *param){
+void *std_assistant(void *param){
 
-	lock = 1;
-	while(lock == 1); //SA sleeping...
+	
+	while(1){
 
+		sem_wait(&sleep);
+
+		
+
+		while(can_sleep == 0);
+
+	}
 	pthread_exit(0);
 }
 
-void student(void *param){
+void *student(void *param){
 
-	lock = 0;
+
+	int *my_id = (int *) param;
+	int help_counter = 0;
+
+	while(help_counter < 3){
+		go_program();
+		help_counter = help_counter + get_help(*my_id);
+	}
+
 	pthread_exit(0);
 }
 
@@ -51,10 +117,14 @@ int main(int argc, char argv[]){
 
 	srand(time(NULL));
 
+	sem_init(&semaphore, 0, 1);
+	sem_init(&sleep, 0, 1);
+	pthread_mutex_init(&mutexLock, NULL);
+
+	sem_wait(&sleep); //the assistant initialize and sleep until the first student appears.
+
 	int amount_std = rand % (MAX_STUDENTS + 1 - MIN_STUDENTS) + MIN_STUDENTS;
 	number_chairs = amount_std/2;
-	chairs = (int *)calloc(number_chairs,sizeof(int));
-
 	int i;
 
 	pthread_t *tid;
@@ -75,6 +145,7 @@ int main(int argc, char argv[]){
 	free(chairs);
 	free(tid);
 	free(attr);
+	pthread_mutex_destroy(&mutexLock);
 
 	return 0;
 }
